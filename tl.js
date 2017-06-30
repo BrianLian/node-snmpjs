1912e43afe6471a3ca81ef2e1ac1ffd6b8ac84da
Eden Sun <huntack478@gmail.com>
2017-06-30 19:32:26 +0800

    make it easier

--- tl.js
+++ tl.js
@@ -2,29 +2,10 @@
 /*
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  */
-
 var snmp = require('./lib/index.js');
-var mib = require('./lib/mib/index.js');
-var bunyan = require('bunyan');
-var fs = require('fs');
 var util = require('util');
 
-var config = process.argv[2] || 'tl.json';
-var cfstr = fs.readFileSync(config);
-var cf, log_cf;
-var log, tl;
-
-cf = JSON.parse(cfstr);
-log_cf = cf.log || {
-	name: 'snmpd',
-	level: 'trace'
-};
-
-log = new bunyan(log_cf);
-
-tl = snmp.createTrapListener({
-	log: log
-});
+tl = snmp.createTrapListener()
 
 tl.on('trap', function (msg) {
 	console.log(util.inspect(snmp.message.serializer(msg), false, null));
