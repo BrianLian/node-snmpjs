1e5635a6f899b4f4450972ea1dcd2d7071d5235e
Eden Sun <huntack478@gmail.com>
2017-06-30 19:42:37 +0800

    rm index depend

--- trapServerExample.js
+++ trapServerExample.js
@@ -2,12 +2,15 @@
 /*
  * Copyright (c) 2013, Joyent, Inc. All rights reserved.
  */
-var snmp = require('./lib/index.js');
+var TrapListener = require('./lib/trap_listener');
+var message = require('./lib/protocol/message');
 var util = require('util');
 
-tl = snmp.createTrapListener()
+tl = new TrapListener({
+	log: console
+})
 
 tl.on('trap', function (msg) {
-	console.log(util.inspect(snmp.message.serializer(msg), false, null));
+	console.log(util.inspect(message.serializer(msg), false, null));
 });
 tl.bind({ family: 'udp4', port: 162 });
